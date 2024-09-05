a9b5bc2097d1f5d48b5d1fc3165b01bb36eed52f
Sandra <sandra.morales@ext.babelgroup.com>
2024-09-05 10:56:31 -0600

    first commit

---
+++ src/main/java/com/example/demo_spring_batch/DemoSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBatchApplication.class, args);
+	}
+
+}
